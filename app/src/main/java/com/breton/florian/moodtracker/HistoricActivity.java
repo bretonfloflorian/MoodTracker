b6aa06af0d846007ca884c24e6a4004c7678a418
Florian <breton.florian@hotmail.com>
2018-03-27 15:37:03 +0200

    Ajout image historique

---
+++ app/src/main/java/com/breton/florian/moodtracker/HistoricActivity.java
@@ -0,0 +1,4 @@
+package com.breton.florian.moodtracker;
+
+public class HistoricActivity {
+}
--- app/src/main/java/com/breton/florian/moodtracker/MainActivity.java
+++ app/src/main/java/com/breton/florian/moodtracker/MainActivity.java
@@ -2,12 +2,32 @@ package com.breton.florian.moodtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImageHumour1;
+    private ImageView mImageHumour2;
+    private ImageView mImageHumour3;
+    private ImageView mImageHumour4;
+    private ImageView mImageHumour5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageHumour1 = (ImageView) findViewById(R.id.image_humour_1);
+        mImageHumour2 = (ImageView) findViewById(R.id.image_humour_2);
+        mImageHumour3 = (ImageView) findViewById(R.id.image_humour_3);
+        mImageHumour4 = (ImageView) findViewById(R.id.image_humour_4);
+        mImageHumour5 = (ImageView) findViewById(R.id.image_humour_5);
+
+        mImageHumour1.setTag(0);
+        mImageHumour2.setTag(1);
+        mImageHumour3.setTag(2);
+        mImageHumour4.setTag(3);
+        mImageHumour5.setTag(4);
+
     }
 }
